7b6b9047573a5cf6559a33ed87425bfe61149667
D-Karak <dibyendu.karak05@gmail.com>
2024-11-08 09:51:18 +0530

    Projects

--- 09. Follow card/script.js
+++ 09. Follow card/script.js
@@ -1,6 +1,14 @@
 // event for changing the textcontent of follow button while clicked
 document.getElementById("follow").addEventListener("click",()=>{
     setTimeout(()=>{
-        follow.textContent="Followed"
+        if(follow.textContent==="Follow"){
+            follow.textContent="Followed";
+        }
+        else{
+            //an alert will show and the the button will unfollowed if the button is followed and clicked again
+           const userName= document.getElementById("user-name").textContent;
+            alert(`You have unfollowd ${userName}`)
+            follow.textContent="Follow";
+        }
     },200)
 });
