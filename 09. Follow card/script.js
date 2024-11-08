2aadf27ea8dc5f69b96aef02bbd735530eb37ad6
D-Karak <dibyendu.karak05@gmail.com>
2024-11-08 09:55:09 +0530

    Projects

--- 09. Follow card/script.js
+++ 09. Follow card/script.js
@@ -7,8 +7,10 @@ document.getElementById("follow").addEventListener("click",()=>{
         else{
             //an alert will show and the the button will unfollowed if the button is followed and clicked again
            const userName= document.getElementById("user-name").textContent;
-            alert(`You have unfollowd ${userName}`)
-            follow.textContent="Follow";
+            let confirmation=confirm(`Are you sure you want to unfllow ${userName} ?`);
+            if(confirmation){
+                follow.textContent="Follow";
+            }
         }
     },200)
 });
